998dc84413844a234ab502fdccbcaf0a3b4cd2f7
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-10-17 13:53:22 -0300

    Added raw CheckBox component

---
+++ FastBlazor/FastComponent.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FastBlazor
+{
+    public class FastComponent
+    {
+        [Parameter]
+        public bool Visible { get; set; }
+    }
+}
---
+++ FastBlazor/FastEditor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace FastBlazor
+{
+    public class FastEditor<T> : FastComponent
+    {
+        private T _value;
+
+        [Parameter]
+        public T Value
+        {
+            get { return _value; }
+            set
+            {
+                if (!EqualityComparer<T>.Default.Equals(_value, value))
+                {
+                    _value = value;
+                }
+            }
+        }
+
+        [Parameter]
+        public EventCallback<T> ValueChanged { get; set; }
+    }
+}
